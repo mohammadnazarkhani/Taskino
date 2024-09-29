fb84a0bf2db97e0064c9277d687de69682424ead
mohammadnazarkhani <m.nazarkhani1380@gmail.com>
2024-09-29 22:23:00 +0330

    HomePage inital setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { React } from "react";
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
 
 const App = () => {
-  return (
-    <div className='text-3xl font-bold underline'>App</div>
-  )
-}
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<HomePage />} />
+            </Route>
+        )
+    );
+    return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
